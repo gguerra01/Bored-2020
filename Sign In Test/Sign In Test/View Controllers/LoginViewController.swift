c5a8c4664c96833312710250c169b76b007208bd
gguerra01 <56694424+gguerra01@users.noreply.github.com>
2020-03-31 01:12:12 -0400

    Labels 2

--- Sign In Test/Sign In Test/View Controllers/LoginViewController.swift
+++ Sign In Test/Sign In Test/View Controllers/LoginViewController.swift
@@ -10,10 +10,29 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        setupElements()
+    }
+    
+    func setupElements(){
+        
+        errorLabel.alpha = 0
+        
     }
     
 
@@ -26,5 +45,9 @@ class LoginViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
 }
--- Sign In Test/Sign In Test/View Controllers/SignUpViewController.swift
+++ Sign In Test/Sign In Test/View Controllers/SignUpViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
+        setupElements()
     }
+    func setupElements() {
+        
+        //Hide error label
+        errorLabel.alpha = 0
+        
+        //Style elements
+        Utilities.styleTextField(firstNameTextField)
+        Utilities.styleTextField(lastNameTextField)
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextField)
+        Utilities.styleFilledButton(signUpButton)
     
 
     /*
@@ -26,5 +51,8 @@ class SignUpViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    @IBAction func signUpTapped(_ sender: Any) {
+    }
+    
 }
