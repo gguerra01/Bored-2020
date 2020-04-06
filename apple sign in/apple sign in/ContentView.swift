aa370839847fcded64b8f9d19ccfc8e78d092e5a
gguerra01 <56694424+gguerra01@users.noreply.github.com>
2020-04-06 19:00:37 -0400

    Apple Sign In

---
+++ apple sign in/apple sign in/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  apple sign in
+//
+//  Created by Gian Guerra on 4/6/20.
+//  Copyright © 2020 Gian Guerra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
